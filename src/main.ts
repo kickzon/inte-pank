4d5289b900e635767b91f2dd8387849b59898ceb
Kickzon <chiara.jukola@gmail.com>
2026-02-05 10:03:24 +0100

    vite config

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,40 @@
+//@ts-nocheck
+
 import './style.css';
+
+let myData = [];
+/***************************************************/
+/********* LÄGG TILL INKOMST OCH UTGIFT ************/
+/***************************************************/
+const addIncomeBtn = document.querySelector('#addIncomeBtn')
+addIncomeBtn?.addEventListener('click', 'kaydown', checkInputConfirm);
+const ENTER_KEY = 13;
+
+function checkInputConfirm(e) {
+  if (e.keyCod !== ENTER_KEY) {
+    return;
+  }
+
+  myData.push({
+    text: input.value,
+  })
+}
+
+
+/***************************************************/
+/**************** SUMMERA INKOMST ******************/
+/***************************************************/
+
+
+
+
+/***************************************************/
+/***************** SUMMERA UTGIFT ******************/
+/***************************************************/
+
+
+
+
+/***************************************************/
+/*********** SUMMERA INKOMST OCH UTGIFT ************/
+/***************************************************/
